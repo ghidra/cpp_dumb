371c3d5ce2770402946fcc061b87c59c90a1686f
ghidra <ghidra@gmail.com>
2015-04-27 21:17:40 -0400

    added endl

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,9 @@
 int main()
 {
     poly * shape = new rectangle(20,20);
-    std::cout << "area:" << shape->area();
+    std::cout << "area:" << shape->area() << std::endl;
+    std::cout << "width:" << shape->get_width() << std::endl;
+    std::cout << "height:" << shape->get_height() << std::endl;
 
     return 0;
 }
